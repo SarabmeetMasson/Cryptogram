f72a3d646802df8dc2ca9927ad8a08e792c56052
Sarabmeet Singh Masson <sarabmeetmasson@gmail.com>
2023-03-29 02:12:48 +0530

    First Commit

--- try.py
+++ try.py
@@ -1,138 +1,74 @@
-# Python program implementing Image Steganography
+import string
 
-# PIL module is used to extract
-# pixels of image and modify it
-from PIL import Image
+# Create the Playfair cipher key table
+def create_table(key):
+    alphabet = string.ascii_lowercase
+    key = key.lower().replace('j', 'i')
+    table = []
+    for c in key + alphabet:
+        if c not in table and c != 'j':
+            table.append(c)
+    return table
 
-# Convert encoding data into 8-bit binary
-# form using ASCII value of characters
-def genData(data):
+# Find the row and column of a given letter in the Playfair cipher key table
+def find_position(table, letter):
+    row = col = 0
+    for i, c in enumerate(table):
+        if letter == c:
+            row, col = divmod(i, 5)
+            break
+    return row, col
 
-		# list of binary codes
-		# of given data
-		newd = []
+# Encrypt a pair of plaintext letters using the Playfair cipher
+def encrypt_pair(table, pair):
+    a, b = pair
+    a_row, a_col = find_position(table, a)
+    b_row, b_col = find_position(table, b)
+    if a_row == b_row: # letters are in the same row
+        return table[a_row*5 + (a_col+1)%5] + table[b_row*5 + (b_col+1)%5]
+    elif a_col == b_col: # letters are in the same column
+        return table[((a_row+1)%5)*5 + a_col] + table[((b_row+1)%5)*5 + b_col]
+    else: # letters form a rectangle
+        return table[a_row*5 + b_col] + table[b_row*5 + a_col]
 
-		for i in data:
-			newd.append(format(ord(i), '08b'))
-		return newd
+# Decrypt a pair of ciphertext letters using the Playfair cipher
+def decrypt_pair(table, pair):
+    a, b = pair
+    a_row, a_col = find_position(table, a)
+    b_row, b_col = find_position(table, b)
+    if a_row == b_row: # letters are in the same row
+        return table[a_row*5 + (a_col-1)%5] + table[b_row*5 + (b_col-1)%5]
+    elif a_col == b_col: # letters are in the same column
+        return table[((a_row-1)%5)*5 + a_col] + table[((b_row-1)%5)*5 + b_col]
+    else: # letters form a rectangle
+        return table[a_row*5 + b_col] + table[b_row*5 + a_col]
 
-# Pixels are modified according to the
-# 8-bit binary data and finally returned
-def modPix(pix, data):
+# Encrypt plaintext using the Playfair cipher
+def encrypt(plaintext, key):
+    plaintext = plaintext.lower().replace('j', 'i').replace(' ', '')
+    table = create_table(key)
+    ciphertext = ''
+    for i in range(0, len(plaintext), 2):
+        pair = plaintext[i:i+2]
+        if len(pair) == 1: # add padding
+            pair += 'x'
+        ciphertext += encrypt_pair(table, pair)
+    return ciphertext.upper()
 
-	datalist = genData(data)
-	lendata = len(datalist)
-	imdata = iter(pix)
+# Decrypt ciphertext using the Playfair cipher
+def decrypt(ciphertext, key):
+    ciphertext = ciphertext.lower().replace('j', 'i').replace(' ', '')
+    table = create_table(key)
+    plaintext = ''
+    for i in range(0, len(ciphertext), 2):
+        pair = ciphertext[i:i+2]
+        plaintext += decrypt_pair(table, pair)
+    return plaintext.upper()
 
-	for i in range(lendata):
-
-		# Extracting 3 pixels at a time
-		pix = [value for value in imdata.__next__()[:3] +
-								imdata.__next__()[:3] +
-								imdata.__next__()[:3]]
-
-		# Pixel value should be made
-		# odd for 1 and even for 0
-		for j in range(0, 8):
-			if (datalist[i][j] == '0' and pix[j]% 2 != 0):
-				pix[j] -= 1
-
-			elif (datalist[i][j] == '1' and pix[j] % 2 == 0):
-				if(pix[j] != 0):
-					pix[j] -= 1
-				else:
-					pix[j] += 1
-				# pix[j] -= 1
-
-		# Eighth pixel of every set tells
-		# whether to stop ot read further.
-		# 0 means keep reading; 1 means thec
-		# message is over.
-		if (i == lendata - 1):
-			if (pix[-1] % 2 == 0):
-				if(pix[-1] != 0):
-					pix[-1] -= 1
-				else:
-					pix[-1] += 1
-
-		else:
-			if (pix[-1] % 2 != 0):
-				pix[-1] -= 1
-
-		pix = tuple(pix)
-		yield pix[0:3]
-		yield pix[3:6]
-		yield pix[6:9]
-
-def encode_enc(newimg, data):
-	w = newimg.size[0]
-	(x, y) = (0, 0)
-
-	for pixel in modPix(newimg.getdata(), data):
-
-		# Putting modified pixels in the new image
-		newimg.putpixel((x, y), pixel)
-		if (x == w - 1):
-			x = 0
-			y += 1
-		else:
-			x += 1
-
-# Encode data into image
-def encode():
-	img = input("Enter image name(with extension) : ")
-	image = Image.open(img, 'r')
-
-	data = input("Enter data to be encoded : ")
-	if (len(data) == 0):
-		raise ValueError('Data is empty')
-
-	newimg = image.copy()
-	encode_enc(newimg, data)
-
-	new_img_name = input("Enter the name of new image(with extension) : ")
-	newimg.save(new_img_name, str(new_img_name.split(".")[1].upper()))
-
-# Decode the data in the image
-def decode():
-	img = input("Enter image name(with extension) : ")
-	image = Image.open(img, 'r')
-
-	data = ''
-	imgdata = iter(image.getdata())
-
-	while (True):
-		pixels = [value for value in imgdata.__next__()[:3] +
-								imgdata.__next__()[:3] +
-								imgdata.__next__()[:3]]
-
-		# string of binary data
-		binstr = ''
-
-		for i in pixels[:8]:
-			if (i % 2 == 0):
-				binstr += '0'
-			else:
-				binstr += '1'
-
-		data += chr(int(binstr, 2))
-		if (pixels[-1] % 2 != 0):
-			return data
-
-# Main Function
-def main():
-	a = int(input(":: Welcome to Steganography ::\n"
-						"1. Encode\n2. Decode\n"))
-	if (a == 1):
-		encode()
-
-	elif (a == 2):
-		print("Decoded Word : " + decode())
-	else:
-		raise Exception("Enter correct input")
-
-# Driver Code
-if __name__ == '__main__' :
-
-	# Calling main function
-	main()
+# Example usage
+key = input("Enter Playfair cipher key: ")
+plaintext = input("Enter plaintext: ")
+ciphertext = encrypt(plaintext, key)
+print("Ciphertext:", ciphertext)
+decrypted = decrypt(ciphertext, key)
+print("Decrypted plaintext:", decrypted)
