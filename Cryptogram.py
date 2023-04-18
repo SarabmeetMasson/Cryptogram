c78d7c5488cdab874626f046fe0e7aefb9027a98
Sarabmeet Singh Masson <sarabmeetmasson@gmail.com>
2023-04-18 11:08:31 +0530

    Update

--- Cryptogram.py
+++ Cryptogram.py
@@ -601,9 +601,35 @@ if action == 1:
             print("Illegal action, try again.")
         if morse_code == 1:
             print("Morse Code Encryption:")
+            morse_code = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', ' ': '/'}
+
+            def to_morse_code(text):
+                morse_text = ''
+                for letter in text.upper():
+                    if letter in morse_code:
+                        morse_text += morse_code[letter] + ' '
+                    else:
+                        morse_text += letter
+                return morse_text
+            user_input = input("Enter a text to Encrypt in Morse Code: ")
+            morse_text = to_morse_code(user_input)
+            print("Morse Code:", morse_text)
 
         elif morse_code == 2:
             print("Morse Code Decryption:")
+            morse_code = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', ' ': '/'}
+            def from_morse_code(morse_text):
+                text = ''
+                morse_to_char = {v: k for k, v in morse_code.items()}
+                for morse_letter in morse_text.split(' '):
+                    if morse_letter in morse_to_char:
+                        text += morse_to_char[morse_letter]
+                    elif morse_letter == '/':
+                        text += ' '
+                return text
+            decryption_input = input("Enter the Morse Code to Decrypt: ")
+            text = from_morse_code(decryption_input)
+            print("Decrypted Text:", text)
 
         else:
             print("Error, Try again.")
