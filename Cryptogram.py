73456b29c73d95040b7c4016d8eaf5e42cc786aa
Sarabmeet Singh Masson <sarabmeetmasson@gmail.com>
2023-05-01 11:25:42 +0530

    Updated

--- Cryptogram.py
+++ Cryptogram.py
@@ -1253,6 +1253,101 @@ while x1 == 1:
 
 
 
+            elif stegano == 3:
+
+                def encode_aud_data():
+                    import wave
+
+                    nameoffile=input("\nEnter the name of the Audio file[with extention]: ")
+                    song = wave.open(nameoffile, mode='rb')
+
+                    nframes=song.getnframes()
+                    frames=song.readframes(nframes)
+                    frame_list=list(frames)
+                    frame_bytes=bytearray(frame_list)
+
+                    data = input("\nEnter the data to be Encoded in Audio: ")
+
+                    res = ''.join(format(i, '08b') for i in bytearray(data, encoding ='utf-8'))     
+                    length = len(res)
+                    data = data + '*^*^*'
+
+                    result = []
+                    for c in data:
+                        bits = bin(ord(c))[2:].zfill(8)
+                        result.extend([int(b) for b in bits])
+
+                    j = 0
+                    for i in range(0,len(result),1): 
+                        res = bin(frame_bytes[j])[2:].zfill(8)
+                        if res[len(res)-4]== result[i]:
+                            frame_bytes[j] = (frame_bytes[j] & 253)      #253: 11111101
+                        else:
+                            frame_bytes[j] = (frame_bytes[j] & 253) | 2
+                            frame_bytes[j] = (frame_bytes[j] & 254) | result[i]
+                        j = j + 1
+                    
+                    frame_modified = bytes(frame_bytes)
+
+                    stegofile=input("\nEnter the name of the Stegano Audio file to be generated[with extention]: ")
+                    with wave.open(stegofile, 'wb') as fd:
+                        fd.setparams(song.getparams())
+                        fd.writeframes(frame_modified)
+                    print("\nEncoded the data successfully in the audio file.")    
+                    song.close()
+
+
+                def decode_aud_data():
+                    import wave
+
+                    nameoffile=input("Enter the name of the Stegano Audio file[with extention]: ")
+                    song = wave.open(nameoffile, mode='rb')
+
+                    nframes=song.getnframes()
+                    frames=song.readframes(nframes)
+                    frame_list=list(frames)
+                    frame_bytes=bytearray(frame_list)
+
+                    extracted = ""
+                    p=0
+                    for i in range(len(frame_bytes)):
+                        if(p==1):
+                            break
+                        res = bin(frame_bytes[i])[2:].zfill(8)
+                        if res[len(res)-2]==0:
+                            extracted+=res[len(res)-4]
+                        else:
+                            extracted+=res[len(res)-1]
+                    
+                        all_bytes = [ extracted[i: i+8] for i in range(0, len(extracted), 8) ]
+                        decoded_data = ""
+                        for byte in all_bytes:
+                            decoded_data += chr(int(byte, 2))
+                            if decoded_data[-5:] == "*^*^*":
+                                print("The Encoded data which was hidden in the Audio was: ",decoded_data[:-5])
+                                p=1
+                                break  
+
+
+                def aud_steg():
+                    x3 = 1
+                    while x3 == 1:
+                        choice1 = int(input("\nAUDIO Stegnography\n1. Encode\n2. Decode\nYou Chose: "))   
+                        if choice1 == 1:
+                            encode_aud_data()
+                        elif choice1 == 2:
+                            decode_aud_data()
+                        else:
+                            print("Illegal action, try again.")
+                        x3 = int(input("Do you want to try again?[1=Yes / 0=No]: "))
+
+
+
+                if __name__ == "__main__":
+                    aud_steg()
+
+
+
             x2 = int(input("Visit Algorithms again?[1=Yes / 0=No]: "))
 
 
@@ -1260,4 +1355,5 @@ while x1 == 1:
         print(attr(1), fg(1),"\nIllegal action, try again.")
 
     x1 = int(input("Revisit main Menu?[1=Yes / 0=No]: "))
+
     
